07438170a7a84e521983aaa0f84641490a0ea4cb
Lê Khắc Thiện <muzankibut112@gmail.com>
2024-09-19 09:13:02 +0700

    Add All Files

---
+++ Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using API_Application.Core.IServices;
+
+namespace API_Application.Services
+{
+    public class UserService : IUserService
+    {
+        public User Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Insert(User u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Update(int id, User u)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
